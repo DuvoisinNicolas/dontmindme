39b007491238e6061a5a0f30ffe2960ff7e85da0
Gr3yM00n <37442663+DuvoisinNicolas@users.noreply.github.com>
2025-01-15 10:40:32 +0100

    Create whoami.bat

---
+++ whoami.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Run `whoami` and save the result to a file in C:\Windows\Tasks
+whoami > C:\Windows\Tasks\whoami_result.txt
+
+:: Optional: Notify the user
+echo The result has been saved to C:\Windows\Tasks\whoami_result.txt
